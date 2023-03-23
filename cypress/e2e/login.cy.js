c34b690338aa7437ff582112597de0b71d5a2f78
Renato Rezende <renatorezende92@hotmail.com>
2023-03-23 19:33:29 -0300

    conexao com banco, usando API

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,73 +2,51 @@
 import login from '../support/pages/login'
 import loginPage from '../support/pages/login'
 import shaversPage from '../support/pages/shavers'
+import data from '../fixtures/users.json'
+
 
 describe('login', () => {
 
     context('quando submeto o formulario', () => {
 
         it('deve logar com sucesso', () => {
-            const user = {
-                name: 'Renato',
-                email: 'renato@hotmail.com',
-                password: '12345678'
-            }
+            const user = data.success
+
+            cy.createUser(user)
 
             loginPage.submit(user.email, user.password)
             shaversPage.header.userShouldBeLoggedIn(user.name)
         })
 
         it('não deve logar com senha incorreta', () => {
-            const user = {
-                name: 'Renato',
-                email: 'renato@hotmail.com',
-                password: '123456'
-            }
+            const user = data.invpass
 
             loginPage.submit(user.email, user.password)
-
             const message = 'Ocorreu um erro ao fazer login, verifique suas credenciais.'
             loginPage.noticeShouldBe(message)
 
         })
 
         it('não deve logar com email não cadastrado', () => {
-            const user = {
-                name: 'Renato',
-                email: 'renato@gmail.com',
-                password: '123456'
-            }
+            const user = data.email404
 
             loginPage.submit(user.email, user.password)
-
             const message = 'Ocorreu um erro ao fazer login, verifique suas credenciais.'
             loginPage.noticeShouldBe(message)
-
         })
 
         it('campos obrigatorios', () => {
             loginPage.submit()
             loginPage.requiredFields('E-mail é obrigatório', 'Senha é obrigatória')
-
         })
-
     })
 
     context('senha muito curta', () => {
 
-        const passwords = [
-            '1',
-            '12',
-            '123',
-            '1234',
-            '12345'
-        ]
-
-        passwords.forEach((p) => {
+        data.shortpass.forEach((p) => {
             it(`não deve logar com a senha: ${p} `, () => {
                 loginPage.submit('renato@teste.com', p)
                 loginPage.alertShouldBe('Pelo menos 6 caracteres')
-
             })
         })
 
@@ -76,20 +54,10 @@ describe('login', () => {
 
     context('email no formato invalido', () => {
 
-        const emails = [
-            'renato.com.br',
-            'renato#com.br',
-            '@',
-            'renato@',
-            '@com.br',
-            '*&%%#%'
-        ]
-
-        emails.forEach((e) => {
+        data.invemails.forEach((e) => {
             it(`não deve logar com o email: ${e} `, () => {
                 loginPage.submit(e, '12345678')
                 loginPage.alertShouldBe('Informe um email válido')
-
             })
         })
 
---
+++ cypress/support/tasks/database.js
@@ -0,0 +1,24 @@
+const { Pool } = require('pg')
+const dbConfig = {
+    host: 'motty.db.elephantsql.com',
+    user: 'rqmknlwd',
+    password: 'V5ieDXca1zYFWK4Mofj41RIhOF_rDg47',
+    database: 'rqmknlwd',
+    port: 5432
+
+}
+
+module.exports = {
+    removeUser(email) {
+        return new Promise(function (resolve) {
+            const pool = new Pool(dbConfig)
+
+            pool.query('DELETE from users where email = $1', [email], function (error, result) {
+                if (error) {
+                    throw error
+                }
+                resolve({ success: result })
+            })
+        })
+    }
+}
