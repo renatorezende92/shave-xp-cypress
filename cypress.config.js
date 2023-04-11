bcf1889881b8ae331d8e88ee03a77b54b2142d66
Renato Rezende <renatorezende92@hotmail.com>
2023-04-11 10:33:23 -0300

    retirando repeticao de localhost do arquivo api

--- cypress.config.js
+++ cypress.config.js
@@ -10,6 +10,10 @@ module.exports = defineConfig({
     },
     viewportWidth: 1920,
     viewportHeight: 1080,
-    baseUrl: 'http://localhost:3000'
+    baseUrl: 'http://localhost:3000',
+    env: {
+      apiUrl: 'http://localhost:3333',
+      apiHelper: 'http://localhost:5000'
+    }
   },
 });
