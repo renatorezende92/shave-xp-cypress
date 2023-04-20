15f0728612f65421cc60e1bde77b71dfaba5e1bd
Renato Rezende <renatorezende92@hotmail.com>
2023-04-20 15:35:10 -0300

    Utilizando o DotEnv e integrando o Tesults

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
+
 const { defineConfig } = require("cypress");
-const { removeUser } = require ('./cypress/support/tasks/database')
+const { removeUser } = require('./cypress/support/tasks/database')
 
 module.exports = defineConfig({
   e2e: {
@@ -10,10 +12,10 @@ module.exports = defineConfig({
     },
     viewportWidth: 1920,
     viewportHeight: 1080,
-    baseUrl: 'http://localhost:3000',
+    baseUrl: process.env.BASE_URL,
     env: {
-      apiUrl: 'http://localhost:3333',
-      apiHelper: 'http://localhost:5000'
+      apiUrl: process.env.API_URL,
+      apiHelper: process.env.API_HELPER
     }
   },
 });
